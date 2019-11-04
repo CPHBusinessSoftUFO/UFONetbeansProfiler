31aa01758eeb141ee6e6dcae3448e8bb9ffd4014
kasperosterbye <kasper.osterbye@gmail.com>
2019-11-04 10:35:24 +0100

    UFO 2019 update

--- src/NetbeansProfiler.java
+++ src/NetbeansProfiler.java
@@ -1,23 +1,27 @@
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The purpose of NetbeansProfiler is to...
+ *
  * @author kasper
  */
 public class NetbeansProfiler {
-    public static void main( String[] args ) {
+
+    public static void main(String[] args) {
         int count = 100;
         int N = 1_000_000;
-        for (int i = 0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             arraylistInsert(N);
         }
     }
-    
-    public static List<Integer> arraylistInsert( int size ) {
+
+    public static List<Integer> arraylistInsert(int size) {
         List<Integer> lst = new ArrayList<>();
-        for (int i = 0; i<size; i++)
+        for (int i = 0; i < size; i++) {
             lst.add(i);
+        }
         return lst;
     }
 }
